ef0523b44ffba235240f6d7819d8b15bede6474b
Boyd Ingalls <boyd.ingalls@gmail.com>
2016-07-13 22:05:04 -0500

    added index variable

--- MN_APCD_ANYS.R
+++ MN_APCD_ANYS.R
@@ -1,8 +1,28 @@
 # MN_APCD Analysis
 
 # read in files
-diag <- read.csv("dPUF2013_diagnosis_v1.0.csv", header = TRUE)
-util <- read.csv("dPUF2013_utilization_v1.0.csv", header = TRUE)
+diag <- read.csv("dPUF2013_diagnosis_v1.0.csv", header = TRUE, 
+                 colClasses = c("factor", "factor", "factor", 
+                                "factor", "factor", "factor", 
+                                "factor", "character", "numeric"))
+options(digits = 12)
+diag$TOTAL_PAID <- sub("$", "", diag$TOTAL_PAID, fixed = TRUE)
+diag$TOTAL_PAID <- as.numeric(as.character(gsub(",","",diag$TOTAL_PAID)))
+
+util <- read.csv("dPUF2013_utilization_v1.0.csv", header = TRUE,
+                 colClasses = c("factor", "factor", "factor",
+                                "factor", "factor", "factor",
+                                "character", "numeric", "numeric"))
+util$TOTAL_PAID <- sub("$", "", util$TOTAL_PAID, fixed = TRUE)
+util$TOTAL_PAID <- as.numeric(as.character(gsub(",","",util$TOTAL_PAID)))
 
 # load libraries
 library(dplyr); library(ggplot2)
+
+
+
+
+# Add variable for $/members and $/services
+diag <- mutate(diag, IND = TOTAL_PAID / DISTINCT_MEMBERS) 
+util <- mutate(util, IND = TOTAL_PAID / DISTINCT_MEMBERS)
+
