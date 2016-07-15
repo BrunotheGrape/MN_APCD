83e4fba74f4598c0f4c047653c2bb3fcfba09383
Boyd Ingalls <boyd.ingalls@gmail.com>
2016-07-14 22:27:16 -0500

    introducing aggregation

--- MN_APCD_ANYS.R
+++ MN_APCD_ANYS.R
@@ -6,14 +6,14 @@ diag <- read.csv("dPUF2013_diagnosis_v1.0.csv", header = TRUE,
                                 "factor", "factor", "factor", 
                                 "factor", "character", "numeric"))
 options(digits = 12)
-diag$TOTAL_PAID <- sub("$", "", diag$TOTAL_PAID, fixed = TRUE)
+diag$TOTAL_PAID <- gsub("$", "", diag$TOTAL_PAID, fixed = TRUE)
 diag$TOTAL_PAID <- as.numeric(as.character(gsub(",","",diag$TOTAL_PAID)))
 
 util <- read.csv("dPUF2013_utilization_v1.0.csv", header = TRUE,
                  colClasses = c("factor", "factor", "factor",
                                 "factor", "factor", "factor",
                                 "character", "numeric", "numeric"))
-util$TOTAL_PAID <- sub("$", "", util$TOTAL_PAID, fixed = TRUE)
+util$TOTAL_PAID <- gsub("$", "", util$TOTAL_PAID, fixed = TRUE)
 util$TOTAL_PAID <- as.numeric(as.character(gsub(",","",util$TOTAL_PAID)))
 
 # load libraries
@@ -26,3 +26,7 @@ library(dplyr); library(ggplot2)
 diag <- mutate(diag, IND = TOTAL_PAID / DISTINCT_MEMBERS) 
 util <- mutate(util, IND = TOTAL_PAID / DISTINCT_MEMBERS)
 
+# subset and split data
+diagmn <- aggregate(diag$IND, list(diag$DX1_CODE, diag$IND), mean)
+diagred <- select(diag, DX1_CODE, IND)
+spdiag <- split(diagred, diag$DX1_CODE)
